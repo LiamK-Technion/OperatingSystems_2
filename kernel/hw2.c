31e10ca10bc71e3fec5c79c201c19ae6834849a7
Liam Koshrovski <liamk@campus.technion.ac.il>
2023-05-29 07:23:53 +0000

    finished hopefully

--- kernel/hw2.c
+++ kernel/hw2.c
@@ -3,7 +3,7 @@
 #include <asm/current.h>
 #include <linux/list.h>
 
-long heaviest_aux(struct task_struct* ptr, pid_t* heaviest_pid, long* max_weight);
+void heaviest_aux(struct task_struct* ptr, long* heaviest_pid, long* max_weight);
 asmlinkage long sys_hello(void) {     
     printk("Hello, World!\n");     
     return 0; 
@@ -24,27 +24,29 @@ asmlinkage long sys_get_weight(void)
 
 asmlinkage long sys_get_ancestor_sum(void)
 {
-    long sum=0;
+    long sum=current->weight;
     struct task_struct* t=current;
-    while (t->pid != 0)
-    {
-        sum=sum + t->weight;
+    if(t->pid ==0)
+        return sum;
+    do{
         t=t->parent;
-    }
-    //do{
-    //    sum=sum+(t->weight);
-    //    t=t->parent;
-    //} while(t->pid != 0);
+        sum=sum + t->weight;
+    }while (t->pid != 0);
     return sum;
 }
 
 asmlinkage long sys_get_heaviest_descendant(void)
 {
+    long heaviest_pid=-1;
+    long max_weight=-1;
     if (list_empty(&current->children))
         return -ECHILD;
-    return heaviest_aux(current, NULL, NULL);}
+    heaviest_aux(current, &heaviest_pid, &max_weight);
+    return heaviest_pid;
+}
 
-long heaviest_aux(struct task_struct* ptr, pid_t* heaviest_pid, long* max_weight)
+
+void heaviest_aux(struct task_struct* ptr, long* heaviest_pid, long* max_weight)
 {
     struct task_struct *task;
     struct list_head *list; 
@@ -52,14 +54,14 @@ long heaviest_aux(struct task_struct* ptr, pid_t* heaviest_pid, long* max_weight
     list_for_each(list, &ptr->children)
     {
         task = list_entry(list, struct task_struct, sibling);
-        if(heaviest_pid==NULL)
+        if(*heaviest_pid==-1)
             *heaviest_pid = task->pid;
-        if(max_weight==NULL)
+        if(*max_weight==-1)
             *max_weight = task->weight;
         if ((task->weight > *max_weight) || ((task->weight == *max_weight) && (task->pid < *heaviest_pid))) {
             *heaviest_pid=task->pid;
             *max_weight=task->weight;
         }
+        heaviest_aux(task, heaviest_pid, max_weight);
     }
-    return *heaviest_pid;
 }
